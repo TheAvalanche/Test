e55f63230e1492328e32468b3206448a760aa5e3
Alex Kartishev <alexkartishev@gmail.com>
2014-07-16 14:24:23 +0300

    Add model

---
+++ src/main/java/com/tele2/model/Occupation.java
@@ -0,0 +1,7 @@
+package com.tele2.model;
+
+public enum Occupation {
+    MANAGER,
+    DEVELOPER,
+    LAZY_ASSHOLE
+}
---
+++ src/main/java/com/tele2/model/Person.java
@@ -0,0 +1,40 @@
+package com.tele2.model;
+
+public class Person {
+    private String name;
+    private String surname;
+    private Integer age;
+    private Occupation occupation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Occupation getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(Occupation occupation) {
+        this.occupation = occupation;
+    }
+}
